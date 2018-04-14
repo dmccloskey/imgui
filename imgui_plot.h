f3d1ad2ce2da9964eed2415aa8f48cde9eb18ddf
Douglas McCloskey <dmccloskey87@gmail.com>
2018-04-14 21:14:39 +0200

    feat: outline for imgui_plot.h

---
+++ imgui_plot.h
@@ -0,0 +1,52 @@
+// # Low level plotting features and components
+
+// ## Interpolation
+// - interpolate
+// - sample
+// - splines
+
+// ## Scales
+// - band 
+// - categorical 
+// - colors 
+// - continuous 
+// - linear 
+// - log 
+// - nice 
+// - ordinal 
+// - pow 
+// - quantile 
+// - quantize 
+// - sequential 
+// - time
+
+// ## Shapes
+// - arc
+// - area
+// - areaRadial
+// - line
+// - lineRadial
+// - pie 
+// - point
+// - pointRadial
+// - stack
+// - symbol
+
+class line
+{
+
+};
+
+// ## Axes
+
+// # High level plotting functions
+
+// ## Charts
+// - 
+// - 
+// - 
+
+// ## Layouts and hierarchies
+// - 
+// - 
+// - 

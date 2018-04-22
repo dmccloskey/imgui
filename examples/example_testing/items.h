89c8cc9080d39eccb5019fe040a1842c595a21e3
Douglas McCloskey <dmccloskey87@gmail.com>
2018-04-22 22:59:39 +1000

    feat: adding filters to table

--- examples/example_testing/items.h
+++ examples/example_testing/items.h
@@ -5,6 +5,9 @@
 #include "imgui.h"
 #include <stdio.h> // vsnprintf, sscanf, printf
 
+#include <iostream>
+#include <string>
+
 // TODO:
 // refactor each widget into its own class
 // parameterize each widget class with attributes that define the contents of the widget
@@ -12,13 +15,22 @@
 
 namespace example
 {
+  // typdefs
+  typedef char element[256];
+  typedef element row[2];
+  typedef bool row_checked[3];
+  
+  // helpers
+
+  // main widgets and widget elements
   static void ShowAppWindow();
   static void ShowAppMainMenuBar();
   static void ShowMenuFile();
   static void SequenceTableWidget(bool* p_open);
   static void TableWidget(bool* p_open);
-  static void TableFilterPopup(const char* popuop_id);
+  static void TableFilterPopup(const char* popuop_id, ImGuiTextFilter filter[], element lines[], bool checked[]);
   static void FileBrowserWidget(bool* p_open);  
+  static void PlotWidget(bool* p_open);  
 
   static void ShowAppWindow()
   {
@@ -45,8 +57,11 @@ namespace example
     static bool show_generic_table = true;
     if (show_generic_table) TableWidget(&show_generic_table);
     
-    static bool show_file_browser = false;
+    static bool show_file_browser = true;
     if(show_file_browser) FileBrowserWidget(&show_file_browser);
+    
+    static bool show_plot = true;
+    if(show_plot) PlotWidget(&show_plot);
 
     if (ImGui::BeginMainMenuBar())
     {
@@ -146,7 +161,7 @@ namespace example
       // - sample_group_name
       static int selected = 0;
       ImGui::BeginChild("left pane", ImVec2(150, 0), true);
-      for (int i = 0; i < 100; i++)
+      for (int i = 0; i < 100; ++i)
       {
         char label[128];
         sprintf(label, "MyObject %d", i);
@@ -179,41 +194,77 @@ namespace example
     {
       // ImGui::BeginChild("##ScrollingRegion", ImVec2(0, ImGui::GetFontSize() * 20), false, ImGuiWindowFlags_HorizontalScrollbar);
 
+      // table data
+      const char* headers[] = {"Name", "Path"};
+      typedef element row[2];
+      row columns[] = {
+        {"One",  "/path/one"},
+        {"Two",  "/path/two"},
+        {"Thre",  "/path/three"},
+      };
+      row_checked columns_checked[] = {
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+      };
+      static ImGuiTextFilter filter[3];
+
       // headers
-      const char* columns[] = {"ID", "Name", "Path", "Hovered"};
-      ImGui::Columns(IM_ARRAYSIZE(columns), "mycolumns", true); // 4-ways, with border
+      ImGui::Columns(IM_ARRAYSIZE(headers) + 1, "mycolumns", true); // 4-ways, with border
       ImGui::Separator();
-      for (int col = 0; col < IM_ARRAYSIZE(columns); ++col)
+      ImGui::Text("Index");
+      ImGui::NextColumn();
+      for (int col = 0; col < IM_ARRAYSIZE(headers); ++col)
       {
-        if (ImGui::Button(columns[col]))
+        if (ImGui::Button(headers[col]))
           ImGui::OpenPopup(&static_cast<char>(col));
-          TableFilterPopup(&static_cast<char>(col));
+          element elements[IM_ARRAYSIZE(columns)]; // extract out column data
+          bool checked[IM_ARRAYSIZE(columns)]; // extract out checked columns
+          for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
+          {
+            *elements[i] = *columns[i][col];
+            checked[i] = columns[i][col];
+          }
+          TableFilterPopup(&static_cast<char>(col), filter, columns, columns_checked);
         ImGui::NextColumn();
       }
 
       // body
       ImGui::Separator();
-      const char* names[3] = { "One", "Two", "Three" };
-      const char* paths[3] = { "/path/one", "/path/two", "/path/three" };
       static int selected = -1;
-      for (int i = 0; i < 3; i++)
+      for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
       {
-        char label[32];
-        sprintf(label, "%04d", i);
-        if (ImGui::Selectable(label, selected == i, ImGuiSelectableFlags_SpanAllColumns))
-            selected = i;
-        bool hovered = ImGui::IsItemHovered();
-        ImGui::NextColumn();
-        ImGui::Text(names[i]); ImGui::NextColumn();
-        ImGui::Text(paths[i]); ImGui::NextColumn();
-        ImGui::Text("%d", hovered); ImGui::NextColumn();
+        bool pass_all_columns = true;
+        for (int j = 0; j < IM_ARRAYSIZE(headers); ++j)
+          if (!filter[j].PassFilter(columns[i][j]))
+            pass_all_columns = false;
+
+        if (pass_all_columns)
+        {
+          char label[32];
+          sprintf(label, "%04d", i);
+          if (ImGui::Selectable(label, selected == i, ImGuiSelectableFlags_SpanAllColumns))
+              selected = i;
+          bool hovered = ImGui::IsItemHovered();
+          ImGui::NextColumn();
+          for (int j = 0; j < IM_ARRAYSIZE(headers); ++j)
+          {
+            ImGui::Text(columns[i][j]);
+            ImGui::NextColumn();
+          }
+        }
       }
       // ImGui::EndChild();
     }
     ImGui::End();
   }  
 
-  static void TableFilterPopup(const char* popuop_id)
+  static void TableFilterPopup(const char* popuop_id, ImGuiTextFilter filter[], row columns[], bool checked[])
   {
     if (ImGui::BeginPopup(popuop_id))
     { 
@@ -230,17 +281,14 @@ namespace example
 
       // Filtering and selecting
       ImGui::Separator();
-      static ImGuiTextFilter filter;
       filter.Draw();
-      const char* lines[] = { "aaa1.c", "bbb1.c", "ccc1.c", "aaa2.cpp", "bbb2.cpp", "ccc2.cpp", "abc.h", "hello, world" };
-      static bool checked[] = { true, true, true, true, true, true, true, true }; 
       static bool check_all = true;
       if (ImGui::Button("check all"))
-        for (int i = 0; i < IM_ARRAYSIZE(lines); i++) checked[i] = true;
+        for (int i = 0; i < IM_ARRAYSIZE(lines); ++i) checked[i] = true;
       ImGui::SameLine();
       if (ImGui::Button("uncheck all"))
-        for (int i = 0; i < IM_ARRAYSIZE(lines); i++) checked[i] = false;
-      for (int i = 0; i < IM_ARRAYSIZE(lines); i++)
+        for (int i = 0; i < IM_ARRAYSIZE(lines); ++i) checked[i] = false;
+      for (int i = 0; i < IM_ARRAYSIZE(lines); ++i)
         if (filter.PassFilter(lines[i]))
           ImGui::Checkbox(lines[i], &checked[i]);
 
@@ -286,8 +334,8 @@ namespace example
 
       // left: current directory folders (recursive tree)
       static int selected_dir = 0;
-      ImGui::BeginChild("Directories", ImVec2(120, 0), true);
-      for (int i = 0; i < 10; i++)
+      ImGui::BeginChild("Directories", ImVec2(120, -ImGui::GetFrameHeightWithSpacing()), true);
+      for (int i = 0; i < 10; ++i)
       {
         char label[128];
         sprintf(label, "MyObject %d", i);
@@ -298,36 +346,42 @@ namespace example
       ImGui::SameLine();
 
       // right: files/folders in highlighted directory
-      ImGui::BeginChild("Current Directory");      
+      ImGui::BeginChild("Current Directory", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true);      
       // headers
-      const char* columns[] = {"Name", "Date Modified", "Type", "Size"};
-      ImGui::Columns(IM_ARRAYSIZE(columns), "mycolumns", true); // 4-ways, with border
-      ImGui::Separator();
-      for (int col = 0; col < IM_ARRAYSIZE(columns); ++col)
+      const char* headers[] = {"Name", "Date Modified", "Type", "Size"};
+      ImGui::Columns(IM_ARRAYSIZE(headers) + 1, "mycolumns", true); // with border
+      ImGui::Separator();      
+      ImGui::Text("index");
+      ImGui::NextColumn();
+      for (int col = 0; col < IM_ARRAYSIZE(headers); ++col)
       {
-        ImGui::Text(columns[col]);
+        ImGui::Text(headers[col]);
         ImGui::NextColumn();
       }
       // Body
       ImGui::Separator();
-      const char* current_dir_names[] = { "file1", "file2", "file3" };
-      const char* date_modified[] = { "04/15/2018 2:00 PM", "04/15/2018 2:00 PM", "04/15/2018 2:00 PM" };
-      const char* type[] = { ".csv", ".txt", ".mzML" };
-      const char* size[] = { "1MB", "2MB", "3MB" };
+      typedef element row[4];
+      row columns[] = {
+        {"file1", "04/15/2018 2:00 PM", ".csv", "1MB"},
+        {"file2", "04/15/2018 2:00 PM", ".txt", "2MB"},
+        {"file3", "04/15/2018 2:00 PM", ".mzML", "3MB"},
+      };
       static int selected_file = -1;
-      for (int i = 0; i < IM_ARRAYSIZE(current_dir_names); i++)
+      for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
       {
-        if (filter.PassFilter(current_dir_names[i]))
+        if (filter.PassFilter(columns[i][0]))
         {
-          char label[64];
-          sprintf(label, current_dir_names[i]);
+          char label[32];
+          sprintf(label, "%04d", i);
           if (ImGui::Selectable(label, selected_file == i, ImGuiSelectableFlags_SpanAllColumns))
               selected_file = i;
           bool hovered = ImGui::IsItemHovered();
           ImGui::NextColumn();
-          ImGui::Text(date_modified[i]); ImGui::NextColumn();
-          ImGui::Text(type[i]); ImGui::NextColumn();
-          ImGui::Text(size[i]); ImGui::NextColumn();
+          for (int j = 0; j < IM_ARRAYSIZE(headers); ++j)
+          {
+            ImGui::Text(columns[i][j]);
+            ImGui::NextColumn();
+          }
         }
       }
       ImGui::EndChild();
@@ -344,7 +398,7 @@ namespace example
       const char* file_types[] = { ".csv", ".txt", ".mzML", "." };
       if (ImGui::BeginMenu("File Type"))
       {
-        for (int i = 0; i < IM_ARRAYSIZE(file_types); i++)
+        for (int i = 0; i < IM_ARRAYSIZE(file_types); ++i)
         {
           ImGui::MenuItem(file_types[i]);
         }
@@ -377,5 +431,101 @@ namespace example
   // Bonus:
   // - allows users to click points include/exclude
   //   from the peak
+
+  static void PlotWidget(bool* p_open)
+  {
+    //TODO: add input for columns and tables (i.e., std::map)
+    ImGui::SetNextWindowSize(ImVec2(500, 440), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Generic plot", p_open))
+    {
+      // left: filters
+      static int selected_dir = 0;
+      char* headers[] = {"sample_name", "sample_type", "component_name"}; // feature or sample columns to filter on
+      typedef element row[3];
+      row columns[] = {
+        {"S1", "A", "C1"},
+        {"S1", "A", "C2"},
+        {"S2", "A", "C1"},
+        {"S2", "A", "C2"},
+        {"S3", "B", "C1"},
+        {"S3", "B", "C2"},
+        {"S4", "B", "C1"},
+        {"S4", "B", "C2"},
+      };
+      typedef bool row_checked[3];
+      row_checked columns_checked[] = {
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+        {true, true, true},
+      };
+      static ImGuiTextFilter filter[3];
+      ImGui::BeginChild("Filters", ImVec2(200, -ImGui::GetFrameHeightWithSpacing()), true);
+      for (int j = 0; j < IM_ARRAYSIZE(headers); ++j)
+      {
+        if (ImGui::Button(headers[j]))
+        {
+          ImGui::OpenPopup(headers[j]);
+        }
+        if (ImGui::BeginPopup(headers[j]))
+        {
+          filter[j].Draw();
+          if (ImGui::Button("check all"))
+            for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
+              columns_checked[i][j] = true;
+          ImGui::SameLine();
+          if (ImGui::Button("uncheck all"))
+            for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
+              columns_checked[i][j] = false;           
+          for (int i = 0; i < IM_ARRAYSIZE(columns); ++i)
+          {
+            bool pass_other_filters = true;
+            for (int k = 0; k < IM_ARRAYSIZE(headers); ++k)
+              if (!columns_checked[i][k])
+                bool pass_other_filters = false; 
+            if (filter[j].PassFilter(columns[i][j]) && pass_other_filters)
+              ImGui::Checkbox(columns[i][j], &columns_checked[i][j]);
+          }
+          ImGui::EndPopup();
+        }
+        ImGui::Separator();
+      }
+      ImGui::EndChild();
+      ImGui::SameLine();
+
+      // right (Tab 1)
+      ImGui::BeginChild("Figure", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true);
+      ImGui::TextWrapped("TODO: add plot");
+      ImGui::EndChild();
+
+      // // right (Tab 2)
+      // ImGui::BeginChild("Figure", ImVec2(0, -ImGui::GetFrameHeightWithSpacing()), true);
+      // ImGui::TextWrapped("TODO: add table");
+      // ImGui::EndChild();
+
+      // bottom: selectd filename, filetypes, open/cancel
+      ImGui::BeginGroup();
+      if (ImGui::Button("Download"))
+      {
+        // TODO download the plot
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("Options"))
+      {
+        // TODO open a table with the raw data
+      }
+      ImGui::SameLine();
+      if (ImGui::Button("New Tab"))
+      {
+        // TODO open a table with the raw data
+      }
+      ImGui::EndGroup();
+    }
+    ImGui::End();
+  } 
 }
 #endif

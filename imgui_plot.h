bf9d14b7112495c51193f210358f4d74d8aff1bf
Douglas McCloskey <dmccloskey87@gmail.com>
2018-04-23 14:36:53 +1000

    fix: restructuring based on feedback

--- imgui_plot.h
+++ imgui_plot.h
@@ -2,107 +2,122 @@
 #include "imgui.h"
 #include "imgui_internal.h"
 
-// ## Data
-template<T a, T b>
-struct PlotDatum
-{
-	a       x;                  // x data (float, int, date, etc)
-    b       y;                  // y data (float, int, date, etc)
-	// ImVec2  pos;                // x and y data (would restrict only to float)
-    float   r;                  // circle radius or box half-width
-    ImU32   stroke_col;
-    float   stroke_width;
-    ImU32   fill_col;
-	int     caption_id;
-    char*   label;              // string identifier for the data point
-    ImFont* label_font;
-    ImU32   label_font_col;
-    float   label_font_size;
-    char*   series;
-};
-
-template<T a, T b>
-typedef ImVector<PlotDatum<a,b>> PlotData; // vector type to store a series of plot data
-
-// ## Plot legends and other features
-struct Legend
-{
-	ImVec2  pos;
-    float   width;
-    float   height;
-    ImU32   col;
-    char*   series;
-    ImFont* series_font;
-    ImU32   series_font_col;
-    float   series_font_size;
-};
-
-struct ColorBar
-{
-	float  span;
-	ImU32  min;
-	ImU32  max;
-};
-
-template<T a, T b>
-struct ErrorBar
-{
-	a       dx1; // left
-	a       dx2; // right
-	b       dy1; // top
-	b       dy2; // bottom
-    bool   shade; // error shades instead of error bars
-};
-template<T a, T b>
-typedef ImVector<ErrorBar<a,b>> ErrorBars; // vector type to store a series of plot data errors
-
 // ## Interpolation
-// moved to math section in imgui_internal.h
 // - interpolate
 // - sample
 // - splines
+static inline time_t    ImLerp(const time_t& a, const time_t& b, const time_t& t);                      //TODO [DM]
+static inline float     ImBSBasis(float t1, float v0, float v1, float v2, float v3);                            //TODO [DM] BSpline basis function
+static inline ImVec4    ImBSBasis(const ImVec4& t1, const ImVec4& v0, const ImVec4& v1, const ImVec4& v2, const ImVec4& v3);                            //TODO [DM] BSpline basis function used for colors
+static inline int       ImBSerp(int* v, float t, bool closed = false);                            //TODO [DM] BSpline interpolate from [a, b) or [a, b] (closed=true)
+static inline float     ImBSerp(float* v, float t, bool closed = false);                        //TODO [DM] BSpline interpolate from [a, b) or [a, b] (closed=true)
+static inline ImVec4    ImBSerp(const ImVec4* v, const ImVec4& t, bool closed = false);                        //TODO [DM] BSpline interpolate used for colors from [a, b) or [a, b] (closed=true)
+static inline int*      ImQuaLerp(int a, int b, float t, int n);                        //TODO [DM] Quantized linear sampler
+static inline float*    ImQuaLerp(float a, float b, float t, int n);                        //TODO [DM] Quantized linear sampler
+static inline time_t*   ImQuaLerp(const time_t& a, const time_t& b, const time_t& t, int n);                        //TODO [DM] Quantized linear sampler
+static inline int*      ImQuaBSerp(int a, int b, float t, int n, bool closed = false);                        //TODO [DM] Quantized BSpline sampler
+static inline float*    ImQuaBSerp(float a, float b, float t, int n, bool closed = false);                        //TODO [DM] Quantized BSpline sampler
+static inline ImVec4*   ImQuaBSerp(const ImVec4& a, const ImVec4& b, const ImVec4& t, int n, bool closed = false);                        //TODO [DM] Quantized BSpline sampler used for colors
+
+// ## Data
+// - plotting interfaces should except the 
+//   following plot elements:
+// a       x;                  // x data (float, int, date, etc)
+// b       y;                  // y data (float, int, date, etc)
+// float   r;                  // circle radius or box half-width
+// ImU32   stroke_col;
+// float   stroke_width;
+// ImU32   fill_col;
+// int     caption_id;
+// char*   label;              // string identifier for the data point
+// ImFont* label_font;
+// ImU32   label_font_col;
+// float   label_font_size;
+// char*   series;	
+// a       dx1; // left
+// a       dx2; // right
+// b       dy1; // top
+// b       dy2; // bottom
+// bool   shade; // error shades instead of error bars
+
+// ## Plot legends and other features
+
+struct ImLegend
+{
+    /**@brief Show a plot legend
+     * 
+     * @param pos Pos of the legend
+     * @param width Width of the legend
+     * @param height Height of the legend
+     * @param col Background color of the legend
+     * @param series List of series labels
+     * @param series_color List of series colors 
+     * @param series_font Font type for series labels
+     * @param series_font_col Color of the series labels
+     * @param series_font_size Font size of the series labels
+     */
+    void ShowLegend(ImVec2 pos, float width, float height, ImU32 col, 
+        char* series[], ImU32 series_color[],
+        ImFont* series_font, ImU32 series_font_col, float series_font_size)
+    {
+        assert(IM_ARRAYSIZE(series) == IM_ARRAYSIZE(series_color));
+        //TODO
+    };
+};
+
+struct ImColorBar
+{
+/**@brief Show a color bar for e.g., a heatmap
+ * 
+ * @param span Range of values to span
+ * @param min Minimum color value
+ * @param max Maximum color value 
+ */
+    void ShowColorBar(const float& span, const ImU32& min, const ImU32& max)
+    {
+        //TODO
+    };
+};
 
 // ## Scales
-struct Scales
+class ImScales
 {
-    ImVec2  domain;     // input data ranges
-    ImVec2  range;      // output data ranges
+public:
+    void Domain(ImVec2 domain);     // input data ranges
+    void Range(ImVec2 range);      // output data ranges
+};
+
+class ImLinearScales: public ImScales
+{
+public:
     // continuous scales
-    void    linear();
-    void    pow();
-    void    log();
-    void    time();
-    void    sequential();
-    void    continuous(); // used for colors
-    // quantized scales
-    void    quantize(); // maps a continouus variable to a discrete scale
-    void    quantile(ImVec2& range, float& val); // maps a continuous varible to a sampled domain
-    void    threshold(); 
-    // discrete scales
-    void    ordered();
-    void    categorical();
-    void    band();
-    void    round();
-    void    point();
+    void    Linear();
+    // void    Pow();
+    // void    Log();
+    // void    Time();
+    // void    Sequential();
+    // void    Continuous(); // used for colors
+    // // quantized scales
+    // void    Quantize(); // maps a continouus variable to a discrete scale
+    // void    Quantile(ImVec2& range, float& val); // maps a continuous varible to a sampled domain
+    // void    Threshold(); 
+    // // discrete scales
+    // void    Ordered();
+    // void    Categorical();
+    // void    Band();
+    // void    Round();
+    // void    Point();
 };
 
 // ## Axes
-struct Axes
+class ImAxes
 {
-    void axesTop();
-    void axesBottom();
-    void axesLeft();
-    void axesRight();
-    float xTickMajor;
-    float xTickMinor;
-    float yTickMajor;
-    float yTickMinor;
-    bool showXAxesGridLines;
-    bool showYAxesGridLines;
-    char* axesTopTitle;
-    char* axesBottomTitle;
-    char* axesLeftTitle;
-    char* axesRightTitle;
+    void ShowAxesTop(char* title, float x_tick_major, float x_tick_minor);
+    void ShowAxesBottom(char* , float x_tick_major, float x_tick_minor);
+    void ShowAxesLeft(char* title, float y_tick_major, float y_tick_minor);
+    void ShowAxesRight(char* title, float y_tick_major, float y_tick_minor);
+    void ShowXAxesGridLines();
+    void ShowYAxesGridLines();
 };
 
 // # High level plotting functions
@@ -130,6 +145,21 @@ struct Axes
 // - Joint
 // - Strip
 // - Swarm
+
+class ImPlotter
+{
+public:
+    ImPlotter(){};
+    ~ImPlotter(){};
+    virtual void ShowPlot();
+};
+
+class ImScatterPlot: public ImPlotter
+{
+public:
+    virtual void ShowPlot() override;
+    void ShowPlot()
+};
 
 
 // ## Layouts and hierarchies
